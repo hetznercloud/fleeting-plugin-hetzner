2c3a9057accd9bee6789e4aec634ddb97099be8e
Axel von Bertoldi <avonbertoldi@gitlab.com>
2022-10-21 08:22:50 -0600

    Fix import alias

--- cmd/fleeting-plugin-aws/main.go
+++ cmd/fleeting-plugin-aws/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	googlecompute "gitlab.com/gitlab-org/fleeting/fleeting-plugin-aws"
+	aws "gitlab.com/gitlab-org/fleeting/fleeting-plugin-aws"
 	"gitlab.com/gitlab-org/fleeting/fleeting/plugin"
 )
 
 func main() {
-	plugin.Serve(&googlecompute.InstanceGroup{})
+	plugin.Serve(&aws.InstanceGroup{})
 }
