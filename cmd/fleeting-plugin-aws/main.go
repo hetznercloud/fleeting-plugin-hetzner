b143a0a97270991213d9e7905d7ea38f18cef23c
Axel von Bertoldi <avonbertoldi@gitlab.com>
2023-03-08 15:35:08 -0700

    Print version info on plugin startup
    This results in the following:
    
        > fleeting-plugin-aws 
        Name:         fleeting-plugin-aws
        Version:      v0.0.1
        Git revision: 249ed16a
        Git branch:   avonbertoldi/12/add-version-information
        GO version:   go1.19.6
        Built:        2023-03-08T22:27:47+0000
        OS/Arch:      linux/amd64
    
        This binary is a plugin. These are not meant to be executed directly.
        Please execute the program that consumes these plugins, which will
        load any plugins automatically

--- cmd/fleeting-plugin-aws/main.go
+++ cmd/fleeting-plugin-aws/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	aws "gitlab.com/gitlab-org/fleeting/fleeting-plugin-aws"
 	"gitlab.com/gitlab-org/fleeting/fleeting/plugin"
 )
 
 func main() {
+	fmt.Println(aws.Version.String())
 	plugin.Serve(&aws.InstanceGroup{})
 }
