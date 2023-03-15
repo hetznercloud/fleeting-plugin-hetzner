e046f576d10c2b6daeeeb25e73e38807fe2bc686
Tomasz Maczukin <tomasz@maczukin.pl>
2023-03-15 22:14:15 +0100

    Pass plugin version info to fleeting through the interface

--- cmd/fleeting-plugin-aws/main.go
+++ cmd/fleeting-plugin-aws/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	aws "gitlab.com/gitlab-org/fleeting/fleeting-plugin-aws"
 	"gitlab.com/gitlab-org/fleeting/fleeting/plugin"
 )
 
+var (
+	showVersion = flag.Bool("version", false, "Show version information and exit")
+)
+
 func main() {
+	flag.Parse()
+	if *showVersion {
+		fmt.Println(aws.Version.Full())
+		os.Exit(0)
+	}
+
 	plugin.Serve(&aws.InstanceGroup{})
 }
