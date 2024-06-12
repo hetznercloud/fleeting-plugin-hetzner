99853806c7376cb84680949ac52aac8b94e30d23
joola <ljonas@riseup.net>
2024-06-12 10:59:59 +0000

    refactor: group and sort properties by concern (hetznercloud/fleeting-plugin-hetzner!82)

--- internal/instancegroup/config.go
+++ internal/instancegroup/config.go
@@ -13,22 +13,22 @@ type Config struct {
 	// `hcloud image list` to list available images.
 	Image string
 
+	// UserData is the data available to initialization framework that may run after the
+	// server boot.
+	UserData string
+
 	// SSHKeys is a list of Hetzner Cloud "SSH Key" (name or id) to create the server
 	// with. Run `hcloud ssh-key list` to list available ssh-keys.
 	SSHKeys []string
-
-	// PrivateNetworks is a list of Hetzner Cloud "Network" (name or id) to attach to
-	// the server. Run `hcloud network list` to list available ssh-keys.
-	PrivateNetworks []string
 
 	// PublicIPv4Disabled disables the server public IPv4.
 	PublicIPv4Disabled bool
 	// PublicIPv6Disabled disables the server public IPv6.
 	PublicIPv6Disabled bool
 
-	// UserData is the data available to initialization framework that may run after the
-	// server boot.
-	UserData string
+	// PrivateNetworks is a list of Hetzner Cloud "Network" (name or id) to attach to
+	// the server. Run `hcloud network list` to list available ssh-keys.
+	PrivateNetworks []string
 
 	// Labels is a map of key value pairs to create the server with.
 	Labels map[string]string
--- provider.go
+++ provider.go
@@ -27,14 +27,16 @@ type InstanceGroup struct {
 	Token    string `json:"token"`
 	Endpoint string `json:"endpoint"`
 
-	Location           string   `json:"location"`
-	ServerType         string   `json:"server_type"`
-	Image              string   `json:"image"`
-	PublicIPv4Disabled bool     `json:"public_ipv4_disabled"`
-	PublicIPv6Disabled bool     `json:"public_ipv6_disabled"`
-	PrivateNetworks    []string `json:"private_networks"`
-	UserData           string   `json:"user_data"`
-	UserDataFile       string   `json:"user_data_file"`
+	Location     string `json:"location"`
+	ServerType   string `json:"server_type"`
+	Image        string `json:"image"`
+	UserData     string `json:"user_data"`
+	UserDataFile string `json:"user_data_file"`
+
+	PublicIPv4Disabled bool `json:"public_ipv4_disabled"`
+	PublicIPv6Disabled bool `json:"public_ipv6_disabled"`
+
+	PrivateNetworks []string `json:"private_networks"`
 
 	sshKey *hcloud.SSHKey
 	labels map[string]string
@@ -105,10 +107,10 @@ func (g *InstanceGroup) Init(ctx context.Context, log hclog.Logger, settings pro
 		Location:           g.Location,
 		ServerType:         g.ServerType,
 		Image:              g.Image,
+		UserData:           g.UserData,
 		PublicIPv4Disabled: g.PublicIPv4Disabled,
 		PublicIPv6Disabled: g.PublicIPv6Disabled,
 		PrivateNetworks:    g.PrivateNetworks,
-		UserData:           g.UserData,
 		Labels:             g.labels,
 	}
 
